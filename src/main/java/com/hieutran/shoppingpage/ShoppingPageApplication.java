0552968e1503acf0f3e0a2e63e6216086df41f9d
Hieu Tran <hieutran6102@gmail.com>
2024-09-28 15:51:05 +0700

    initial project

---
+++ src/main/java/com/hieutran/shoppingpage/ShoppingPageApplication.java
@@ -0,0 +1,13 @@
+package com.hieutran.shoppingpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hieutran/shoppingpage/ShoppingPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieutran.shoppingpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
