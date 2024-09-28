af4f17d7b7783065f7cf0b10e7c314cec34d1b71
Hieu Tran <hieutran6102@gmail.com>
2024-09-29 00:11:46 +0700

    feat: draft controller, service, repository

---
+++ src/main/java/com/hieutran/shoppingpage/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.hieutran.shoppingpage.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+}
---
+++ src/main/java/com/hieutran/shoppingpage/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.hieutran.shoppingpage.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+}
